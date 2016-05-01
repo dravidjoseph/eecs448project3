857ba22e0b0f4b27030d4d297faa0a7f9bebb5bb
Jinuk Park <kebiya93@gmail.com>
2016-05-01 15:30:50 -0500

    fix an error

--- search.php
+++ search.php
@@ -3,35 +3,46 @@
 
 $searchInput = urlencode($_POST["input"]);
 
-
-$searchUrl = "https://api-public.guidebox.com/v1.43/US/rKrgT4qOQA2NmwWX5riPZETUlqVpkuNj/search/movie/title/$searchInput/fuzzy";
+$searchUrl      = "https://api-public.guidebox.com/v1.43/US/rKrgT4qOQA2NmwWX5riPZETUlqVpkuNj/search/movie/title/$searchInput/fuzzy";
 $searchResponse = file_get_contents($searchUrl);
-$searchObj = json_decode($searchResponse, true);
+$searchObj      = json_decode($searchResponse, true);
 
-$searchShowUrl = "https://api-public.guidebox.com/v1.43/US/rKrgT4qOQA2NmwWX5riPZETUlqVpkuNj/search/title/$searchInput/fuzzy";
+$searchShowUrl      = "https://api-public.guidebox.com/v1.43/US/rKrgT4qOQA2NmwWX5riPZETUlqVpkuNj/search/title/$searchInput/fuzzy";
 $searchShowResponse = file_get_contents($searchShowUrl);
-$searchShowObj = json_decode($searchShowResponse, true);
+$searchShowObj      = json_decode($searchShowResponse, true);
 
- echo '<html><head>
+function printMovies($searchObj)
+{
+    $i = 0;
+    while (isset($searchObj[results][$i][title])) {
+        echo '<form action="movieInfo.php" method="post" target="_blank">
+        ', $searchObj[results][$i][title], ' (', $searchObj[results][$i][release_year], ')<input type="text" hidden value="', $searchObj[results][$i][id], '" name="movieID" ><br>
+        <input type="submit" value = "More Information">
+    </form>';
+        $i++;
+    }
+    
+}
+
+function printShows($searchShowObj)
+{
+    $i = 0;
+    while (isset($searchShowObj[results][$i][title])) {
+        echo '<form action="showInfo.php" method="post" target="_blank">
+        ', $searchShowObj[results][$i][title], ' (', $searchShowObj[results][$i][first_aired], ')<input type="text" hidden value="', $searchShowObj[results][$i][id], '" name="movieID" ><br>
+        <input type="submit" value = "More Information">
+    </form>';
+        $i++;
+    }
+}
+
+echo '<html><head>
       </head><body>';
-      echo '*****MOVIES*****';
-$i = 0; 
-   
-while (isset($searchObj[results][$i][title])) {	
-	echo '<form action="movieInfo.php" method="post" target="_blank">
-        ',$searchObj[results][$i][title],'<input type="text" hidden value="',$searchObj[results][$i][id],'" name="movieID" ><br>
-        <input type="submit" value = "More Information">
-    </form>';
-	$i++;
-}
-echo '*****SHOWS*****';
-while (isset($searchShowObj[results][$i][title])) {	
-	echo '<form action="showInfo.php" method="post" target="_blank">
-        ',$searchShowObj[results][$i][title],'<input type="text" hidden value="',$searchShowObj[results][$i][id],'" name="movieID" ><br>
-        <input type="submit" value = "More Information">
-    </form>';
-	$i++;
-}
+echo '<h>*****MOVIES*****</h>';
+printMovies($searchObj);
+
+echo '<h>*****SHOWS*****</h>';
+printShows($searchShowObj);
 
 echo '</body></html>';
 ?>
