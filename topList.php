ae9bce484c25551d61d739e33f34cd8b3733a47c
Jinuk Park <kebiya93@gmail.com>
2016-05-04 16:40:51 -0500

    make topList retrieve less info

--- topList.php
+++ topList.php
@@ -1,18 +1,20 @@
 <?php
 
+
 function tableMovies()
 {
-    $movieListUrl      = "https://api-public.guidebox.com/v1.43/US/rKrgT4qOQA2NmwWX5riPZETUlqVpkuNj/movies/all/1/10/all/all";
+    $numinList = 6;
+    $movieListUrl      = "https://api-public.guidebox.com/v1.43/US/rKrgT4qOQA2NmwWX5riPZETUlqVpkuNj/movies/all/0/".$numinList."/all/all";
     $movieListResponse = file_get_contents($movieListUrl);
     $movieListObj      = json_decode($movieListResponse, true);
-    $numMovies = 6;
+    
     echo '<tr>';
-    for ($i = 0; $i < $numMovies; $i++) {
+    for ($i = 0; $i < $numinList; $i++) {
         echo '<td><a href="movieInfo.php? id= ',$movieListObj[results][$i][id],' " ><img src = "', $movieListObj[results][$i][poster_240x342],'" class = "posters"/></a></td>';
     }
     echo '</tr>';
     echo '<tr>';
-    for ($i = 0; $i < $numMovies; $i++) {
+    for ($i = 0; $i < $numinList; $i++) {
         echo '<td>', $movieListObj[results][$i][title], '</td>';
     }
     echo '</tr>';
@@ -20,17 +22,17 @@ function tableMovies()
 
 function tableShows()
 {
-    $showListUrl      = "https://api-public.guidebox.com/v1.43/US/rKrgT4qOQA2NmwWX5riPZETUlqVpkuNj/shows/all/1/10/all/all";
+    $numinList = 6;
+    $showListUrl      = "https://api-public.guidebox.com/v1.43/US/rKrgT4qOQA2NmwWX5riPZETUlqVpkuNj/shows/all/0/".$numinList."/all/all";
     $showListResponse = file_get_contents($showListUrl);
     $showListObj      = json_decode($showListResponse, true);
-    $numShows = 6;
     echo '<tr>';
-    for ($i = 0; $i < $numShows; $i++) {
+    for ($i = 0; $i < $numinList; $i++) {
         echo '<td><a href="showInfo.php? id= ',$showListObj[results][$i][id],' " ><img src = "', $showListObj[results][$i][artwork_208x117], '" class = "posters" /></a></td>';
     }
     echo '</tr>';
     echo '<tr>';
-    for ($i = 0; $i < $numShows; $i++) {
+    for ($i = 0; $i < $numinList; $i++) {
         echo '<td>', $showListObj[results][$i][title], '</td>';
     }
     echo '</tr>';
