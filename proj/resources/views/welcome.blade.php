2dbc5b3414f64d19c778cadb443bda752358f044
Francinaldo Almeida <fap_sb@globomail.com>
2016-05-09 16:11:06 -0500

    Change landing page

--- proj/resources/views/welcome.blade.php
+++ proj/resources/views/welcome.blade.php
@@ -3,14 +3,21 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
-
-                <div class="panel-body">
-                    Your Application's Landing Page.
-                </div>
+        <div class="col-md-10 col-md-offset-1 text-center">
+          <h1>Wanna know how to watch your favorite movie?</h1>
+          <p>Check it out right now!</p>
+          <br>
+          <br>
+          <div class="form-group input-lg" align="center">
+            <div class="input-group">
+              <form class="input-group" action="search.php" method="post">
+                <input type="text" class="form-control input-lg" placeholder="Type the movie name" name="search">
+                <span class="input-group-btn">
+                  <input class="btn btn-default btn-lg" type="submit" value = "Search"></input>
+                </span>
+              </form>
             </div>
+          </div>
         </div>
     </div>
 </div>
