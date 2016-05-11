ec0303b11fb0e5f7c87da7ea6f1aa19ec09937ec
Jinuk Park <kebiya93@gmail.com>
2016-05-10 22:24:45 -0500

    movie.blade.php

--- proj/resources/views/movie.blade.php
+++ proj/resources/views/movie.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-4">
             <img src="{{$movie['poster_240x342']}}" class="center-block img-responsive">
-            <h1>{{$movie['title']}}({{$movie['release_year']}})</h1>
+            <h1>{{$movie['title']}} ({{$movie['release_year']}})</h1>
             <p>Length: {{gmdate("H:i:s",$movie['duration'])}}</p>
             <p>{{$movie['overview']}}</p>
             <p></p>
@@ -14,7 +14,11 @@
             <h1>How can you watch?</h1>
             <p></p>
             <ul class="media-list">
-              @foreach ($movie['purchase_web_sources'] as $source)
+              @if ($movie['purchase_web_sources'] != null)
+                @foreach ($movie['purchase_web_sources'] as $source)
+              @else
+                {{$movie['purchase_web_sources']['source']}}{{$movie['purchase_web_sources']['display_name'}}{{$movie['purchase_web_sources']['link'}}
+              @endif
               <li class="media">
                 <a href="{{$source['link']}}" class="pull-left" target="_blank"><i class="fa fa-3x fa-fw fa-play-circle"></i></a>
                 <div class="media-body">
