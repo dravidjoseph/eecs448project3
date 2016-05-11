b3ba14f11c2a43f22e549cbcf2c4d26523a8767c
Francinaldo Almeida <fap_sb@globomail.com>
2016-05-11 13:08:17 +0000

    Finish Tests

--- proj/tests/ExampleTest.php
+++ proj/tests/ExampleTest.php
@@ -6,106 +6,61 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
+    use DatabaseTransactions;
     
     /** Index (main) page testing **/
-    
-    public function testTitle()
-    {
-        $this->visit('/')
-             ->see('Rock Chalk JayMovies');
-    }
-    
-    public function testTitleHome()
-    {
-        $this->visit('/')
-             ->see('Home');
-    }
-    
-    public function testTitleLogin()
-    {
-        $this->visit('/')
-             ->see('Login');
-    }
-    
-    public function testTitleRegister()
-    {
-        $this->visit('/')
-             ->see('Register');
-    }
-    
-    public function testTitleShows()
-    {
-        $this->visit('/')
-             ->see('Shows');
-    }
     
     public function testHeader()
     {
         $this->visit('/')
-             ->see('Wanna know how to watch your favorite movie?');
-    }
-    
-    public function testParagraph()
-    {
-        $this->visit('/')
-             ->see('Check it out right now!');
-    }
-    
-    public function testSearch()
-    {
-        $this->visit('/')
-             ->see('Search');
-    }
-    
-    public function testInputBox()
-    {
-        $this->visit('/')
-             ->see('Type the movie name');
+             ->see('Rock Chalk JayMovies')
+             ->see('Home')
+             ->see('Login')
+             ->see('Register')
+             ->see('Shows')
+             ->see('Movies');
     }
     
     public function testSearchButtonEmpty()
     {
-        $this->visit('/')
+        $user = factory(App\User::class)->create();
+
+        $this->actingAs($user)
+             ->visit('/')
+             ->see('Type the movie name')
              ->press('Search')
-             ->seePageIs('/')
              ->see('The search field is required.');
     }
-    
-    public function testSearchRandom()
-    {
-        $this->visit('/')
-             ->type('lkajsdkljflasjdfj', 'search')
-             ->press('Search')
-             ->seePageIs('/search');
-    }
-    
+
     public function testSearchButtonValid()
     {
-        $this->visit('/')
+        $user = factory(App\User::class)->create();
+
+        $this->actingAs($user)
+             ->visit('/')
              ->type('Star Wars', 'search')
              ->press('Search')
              ->seePageIs('/search');
     }
-    
+ 
     public function testRCJButton()
     {
-        $this->visit('/')
+        $user = factory(App\User::class)->create();
+
+        $this->actingAs($user)
+             ->visit('/')
              ->click('Rock Chalk JayMovies')
              ->seePageIs('/');
     }
     
     public function testShowsButton()
     {
-        $this->visit('/')
+        $user = factory(App\User::class)->create();
+
+        $this->actingAs($user)
+             ->visit('/')
              ->click('Shows')
              ->seePageIs('/shows');
-    }
-    
-    public function testHomeButton()
-    {
-        $this->visit('/')
-             ->click('Home')
-             ->seePageIs('/login');
     }
     
     public function testLoginButton()
@@ -125,40 +80,14 @@ class ExampleTest extends TestCase
     
     /** Login page testing **/
     
-    public function testLoginEmailTitle()
+    public function testLoginFields()
     {
         $this->visit('/login')
-             ->see('E-Mail Address');
-    }
-    
-    public function testPasswordTitle()
-    {
-        $this->visit('/login')
-             ->see('Password');
-    }
-    
-    public function testRememberMeTitle()
-    {
-        $this->visit('/login')
-             ->see('Remember Me');
-    }
-    
-    public function testFYPTitle()
-    {
-        $this->visit('/login')
-             ->see('Forgot Your Password?');
-    }
-    
-    public function testLoginFBTitle()
-    {
-        $this->visit('/login')
+             ->see('E-Mail Address')
+             ->see('Password')
+             ->see('Remember Me')
+             ->see('Forgot Your Password?')
              ->see('Login with Facebook');
-    }
-    
-    public function testCheckBox()
-    {
-        $this->visit('/login')
-             ->check('remember');
     }
     
     public function testEmptyEmailPassword()
@@ -196,20 +125,19 @@ class ExampleTest extends TestCase
              ->seePageIs('/login');
     }
     
-    /**
     public function testValidEmailPassword()
     {
+        $user = factory(App\User::class)->create([
+            "name" => "Test Name",
+            "password" => bcrypt("123456")
+        ]);
+
         $this->visit('/login')
-             ->type('abc@gmail.com', 'email')
-             ->type('12345', 'password')
+             ->type($user->email, 'email')
+             ->type("123456", 'password')
              ->press('Login')
-             ->seePageIs('/login');
+             ->seePageIs('/');
     }
-    
-    Add function for checked box + correct input
-    Ask abaout rechecking the nav bar links
-    
-    **/
     
     public function testFYPLink()
     {
@@ -220,21 +148,11 @@ class ExampleTest extends TestCase
     
     /** Forgot password page testing **/
     
-    public function testResetPassTitle()
+    public function testResetFields()
     {
         $this->visit('/password/reset')
-             ->see('Reset Password');
-    }
-    
-    public function testResetEmailTitle()
-    {
-        $this->visit('/password/reset')
-             ->see('E-Mail Address');
-    }
-    
-    public function testSendPassResetTitle()
-    {
-        $this->visit('/password/reset')
+             ->see('Reset Password')
+             ->see('E-Mail Address')
              ->see('Send Password Reset Link');
     }
     
@@ -254,45 +172,15 @@ class ExampleTest extends TestCase
              ->seePageIs('/password/reset');
     }
     
-    /** Fix seePageIs when updated
-    public function testValidResetEmail()
-    {
-        $this->visit('/password/reset')
-             ->type('abc@gmail.com', 'email')
-             ->press('Send Password Reset Link')
-             ->seePageIs('/');
-    }
-    **/
-    
     /** Register page testing **/
     
-    public function testRegisterTitle()
+    public function testRegisterFields()
     {
         $this->visit('/register')
-             ->see('Register');
-    }
-    
-    public function testRegisterNameTitle()
-    {
-        $this->visit('/register')
-             ->see('Name');
-    }
-    
-    public function testRegisterEmailTitle()
-    {
-        $this->visit('/register')
-             ->see('E-Mail Address');
-    }
-    
-    public function testRegisterPasswordTitle()
-    {
-        $this->visit('/register')
-             ->see('Password');
-    }
-    
-    public function testRegisterConfirmTitle()
-    {
-        $this->visit('/register')
+             ->see('Register')
+             ->see('Name')
+             ->see('E-Mail Address')
+             ->see('Password')
              ->see('Confirm Password');
     }
     
@@ -306,15 +194,12 @@ class ExampleTest extends TestCase
              ->seePageIs('/register');
     }
     
-    
-    /**  Need email page to work. If enter any valid email it redirects no matter what.
-    
     public function testRegisterNoName()
     {
         $this->visit('/register')
              ->type('abc@gmail.com', 'email')
-             ->type('12345', 'password')
-             ->type('12345', 'password_confirmation')
+             ->type('123456', 'password')
+             ->type('123456', 'password_confirmation')
              ->press('Register')
              ->see('The name field is required.')
              ->seePageIs('/register');
@@ -324,8 +209,8 @@ class ExampleTest extends TestCase
     {
         $this->visit('/register')
              ->type('Richard', 'name')
-             ->type('12345', 'password')
-             ->type('12345', 'password_confirmation')
+             ->type('123456', 'password')
+             ->type('123456', 'password_confirmation')
              ->press('Register')
              ->see('The email field is required.')
              ->seePageIs('/register');
@@ -346,7 +231,7 @@ class ExampleTest extends TestCase
         $this->visit('/register')
              ->type('Richard', 'name')
              ->type('abc@gmail.com', 'email')
-             ->type('12345', 'password')
+             ->type('123456', 'password')
              ->press('Register')
              ->see('The password confirmation does not match.')
              ->seePageIs('/register');
@@ -357,8 +242,8 @@ class ExampleTest extends TestCase
         $this->visit('/register')
              ->type('Richard', 'name')
              ->type('abc@gmail.com', 'email')
-             ->type('12345', 'password')
-             ->type('1234', 'password_confirmation')
+             ->type('123456', 'password')
+             ->type('123457', 'password_confirmation')
              ->press('Register')
              ->see('The password confirmation does not match.')
              ->seePageIs('/register');
@@ -369,183 +254,82 @@ class ExampleTest extends TestCase
         $this->visit('/register')
              ->type('Richard', 'name')
              ->type('abc', 'email')
-             ->type('12345', 'password')
-             ->type('12345', 'password_confirmation')
+             ->type('123456', 'password')
+             ->type('123456', 'password_confirmation')
              ->press('Register')
              ->seePageIs('/register');
     }
     
     public function testRegisterValid()
     {
+        $user = factory(App\User::class)->make();
+
         $this->visit('/register')
-             ->type('Richard', 'name')
-             ->type('abc@gmail.com', 'email')
-             ->type('12345', 'password')
-             ->type('12345', 'password_confirmation')
+             ->type($user->name, 'name')
+             ->type($user->email, 'email')
+             ->type('123456', 'password')
+             ->type('123456', 'password_confirmation')
              ->press('Register')
-             ->seePageIs('/');  Update to correct url when updated
+             ->seePageIs('/');
     }
-    **/
     
     /** Search page testing **/
     
     public function testSearchMovieTitle()
     {
-        $this->visit('/')
+        $user = factory(App\User::class)->create();
+
+        $this->actingAs($user)
+             ->visit('/')
              ->type('Star Wars', 'search')
              ->press('Search')
-             ->see('Star Wars');
-    }
-    
-    public function testSearchMoreInfoTitle() 
-    {
-        $this->visit('/')
-             ->type('Star Wars', 'search')
-             ->press('Search')
-             ->see('More Information');
-    }
-    
-    public function testSearchMoreInfoButton()
-    {
-        $this->visit('/')
-             ->type('Star Wars', 'search')
-             ->press('Search')
+             ->see('Star Wars')
+             ->see('More Information')
              ->click('More Information')
-             ->seePageIs('/movie/133474');
-    }
-    
-    
-    /** Search results testing **/
-    
-    public function testResultsMovieTitle()
-    {
-        $this->visit('/')
-             ->type('Star Wars', 'search')
-             ->press('Search')
-             ->click('More Information')
-             ->see('Star Wars');
-    }
-    
-    public function testResultsHCYWTitle()   
-    {
-        $this->visit('/')
-             ->type('Star Wars', 'search')
-             ->press('Search')
-             ->click('More Information')
-             ->see('How can you watch?');
-    }
-    
-    public function testResultsParagraph()
-    {
-        $this->visit('/')
-             ->type('Star Wars', 'search')
-             ->press('Search')
-             ->click('More Information')
+             ->seePageIs('/movie/133474')
+             ->see('How can you watch?')
              ->see('Length')
-             ->see('Lucasfilm');
-    }
-    
-    public function testResultsLinkTitle()
-    {
-        $this->visit('/')
-             ->type('Star Wars', 'search')
-             ->press('Search')
-             ->click('More Information')
+             ->see('Lucasfilm')
              ->see('iTunes');
     }
-    /** FIgure out how to test
-    public function testResultsPlayButton()
-    {
-        $this->visit('/')
-             ->type('Star Wars', 'search')
-             ->press('Search')
-             ->click('More Information')
-             ->click('.fa-play-circle');
-             
-    }**/
-    
     
     /** Shows page testing **/
     
     public function testShowsHeader()
     {
-        $this->visit('/')
+        $user = factory(App\User::class)->create();
+
+        $this->actingAs($user)
+             ->visit('/')
              ->click('Shows')
-             ->see('Wanna know how to watch your favorite show?');
-    }
-    
-    public function testShowsParagraph()
-    {
-        $this->visit('/')
-             ->click('Shows')
-             ->see('Check it out right now!');
-    }
-    
-    public function testShowsSearch()
-    {
-        $this->visit('/')
-             ->click('Shows')
-             ->see('Search');
-    }
-    
-    public function testShowsTextBox()
-    {
-        $this->visit('/')
-             ->click('Shows')
+             ->see('Wanna know how to watch your favorite show?')
+             ->see('Check it out right now!')
+             ->see('Search')
              ->see('Type the show name');
     }
     
     public function testShowsSearchButtonEmpty()
     {
-        $this->visit('/shows')
+        $user = factory(App\User::class)->create();
+
+        $this->actingAs($user)
+             ->visit('/shows')
              ->press('Search')
              ->seePageIs('/shows')
              ->see('The search field is required.');
     }
     
-    public function testShowsSearchRandom()
-    {
-        $this->visit('/shows')
-             ->type('lkajsdkljflasjdfj', 'search')
-             ->press('Search')
-             ->seePageIs('/shows/search');
-    }
-    
     public function testShowsSearchButtonValid()
     {
-        $this->visit('/shows')
+        $user = factory(App\User::class)->create();
+
+        $this->actingAs($user)
+             ->visit('/shows')
              ->type('the walking dead', 'search')
              ->press('Search')
-             ->seePageIs('/shows/search');
+             ->seePageIs('/shows/search')
+             ->see('the walking dead')
+             ->see('More Information')
+             ->click('More Information');
     }
-    
-    /** Shows Search page testing **/
-    
-    public function testShowsSearchMovieTitle()
-    {
-        $this->visit('/shows')
-             ->type('the walking dead', 'search')
-             ->press('Search')
-             ->see('the walking dead');
-    }
-    
-    public function testShowsSearchMoreInfoTitle()   
-    {
-        $this->visit('/shows')
-             ->type('the walking dead', 'search')
-             ->press('Search')
-             ->see('More Information');
-    }
-    /** wait for update 
-    public function testShowsSearchMoreInfoButton()
-    {
-        $this->visit('/shows')
-             ->type('the walking dead', 'search')
-             ->press('Search')
-             ->click('More Information')
-             ->seePageIs('/);
-    }
-    **/
-    
-    
 }
--- proj/tests/TestCase.php
+++ proj/tests/TestCase.php
@@ -7,7 +7,7 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
      *
      * @var string
      */
-    protected $baseUrl = 'http://localhost';
+    protected $baseUrl = 'http://eecs448.f5tech.com.br';
 
     /**
      * Creates the application.
