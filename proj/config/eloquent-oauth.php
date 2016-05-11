bec0f0b9e03e95f461703c5875176a66239822dd
Francinaldo Almeida <fap_sb@globomail.com>
2016-05-10 21:07:47 -0500

    Change path

--- proj/config/eloquent-oauth.php
+++ proj/config/eloquent-oauth.php
@@ -9,13 +9,13 @@ return [
         'facebook' => [
             'client_id' => '1118003061584649',
             'client_secret' => '721d57e9fa83afe680217871115a9e8d',
-            'redirect_uri' => 'http://localhost:8000/facebook/login',
+            'redirect_uri' => env("APP_URL").'/facebook/login',
             'scope' => [],
         ],
         'github' => [
             'client_id' => 'e5950068c8e9d9473098',
             'client_secret' => 'c70ec58ea8526cb146c24d91cc100e9c0fd1a837',
-            'redirect_uri' => 'http://localhost:8000/github/login',
+            'redirect_uri' => env("APP_URL").'github/login',
             'scope' => [],
         ],
     ],
