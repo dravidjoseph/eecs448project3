8b343c9cd04edef60611ac68a79e0b1188b17cae
Jinuk Park <kebiya93@gmail.com>
2016-05-10 20:53:36 -0500

    add movies tab

--- proj/resources/views/layouts/app.blade.php
+++ proj/resources/views/layouts/app.blade.php
@@ -50,6 +50,7 @@
                 <ul class="nav navbar-nav">
                     <li><a href="{{ url('/home') }}">Home</a></li>
                     <li><a href="{{ url('/shows') }}">Shows</a></li>
+                    <li><a href="{{ url('/') }}">Movies</a></li>
                 </ul>
 
                 <!-- Right Side Of Navbar -->
