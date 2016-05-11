66e32704427f2d7d24bd76ae52c2c8ade97db4a0
Francinaldo Almeida <fap_sb@globomail.com>
2016-05-11 13:14:06 +0000

    Take out Home

--- proj/resources/views/layouts/app.blade.php
+++ proj/resources/views/layouts/app.blade.php
@@ -48,7 +48,6 @@
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/home') }}">Home</a></li>
                     <li><a href="{{ url('/shows') }}">Shows</a></li>
                     <li><a href="{{ url('/') }}">Movies</a></li>
                 </ul>
