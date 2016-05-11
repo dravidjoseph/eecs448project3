4066dfb744f80ed193603d0225089183d5cf5539
Dravid Joseph <dravidjoseph@gmail.com>
2016-05-10 21:21:45 -0500

    Add login

--- proj/resources/views/auth/login.blade.php
+++ proj/resources/views/auth/login.blade.php
@@ -51,9 +51,14 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
+                                    <i class="fa fa-btn fa-sign-in"></i><a href = "facebook/authorize">Login with Facebook</a>
+                                </button>
+								<a href = "facebook/authorize">
+                                <button class="btn btn-primary">
                                     <i class="fa fa-btn fa-sign-in"></i>Login with Facebook
                                 </button>
-									<a href = "facebook/authorize">Login Test</a>
+								</a>
+									
 
                                 <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
                             </div>
